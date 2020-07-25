bbcba0536a614b79f86b1ef466fdf5a48504a4a4
Brock Ryan <brock.ryan2@gmail.com>
2020-07-25 16:53:15 -0400

    refactor button creation and layout in GUI

--- main.py
+++ main.py
@@ -3,21 +3,21 @@
 #from calculator import Calculator
 from tkinter import *
 
-CANVAS_WIDTH  = 300
-CANVAS_HEIGHT = 600
+CANVAS_WIDTH  = 350
+CANVAS_HEIGHT = 500
 CANVAS_COLOR  = "light gray"
 
-DISPLAY_WIDTH = 0.97
-DISPLAY_HEIGHT= 0.10
+DISPLAY_WIDTH = 0.96
+DISPLAY_HEIGHT= 0.15
 DISPLAY_X     = 0.50
 DISPLAY_Y     = 0.02
 DISPLAY_ANCHOR= 'n'
 DISPLAY_COLOR = "#E4F3FA"
 
-BUTTONfRAME_WIDTH  = 0.97
+BUTTONfRAME_WIDTH  = 0.96
 BUTTONfRAME_HEIGHT = 0.84
 BUTTONfRAME_X      = 0.50
-BUTTONfRAME_Y      = 0.14
+BUTTONfRAME_Y      = 0.20
 BUTTONfRAME_ANCHOR = 'n'
 BUTTONfRAME_COLOR  = "light gray"
 
@@ -38,50 +38,61 @@ DOUBLEBUTTON_COLOR = "blue"
 BUTTON_FONT = 40
 BUTTON_GAP  = 5
 
-BUTTON_TEXT=["MC", "MR", "MS", "M+", "M-", "AC", "C", "+/-", "^2", "^y", "7", "8", "9", "/", "sqrt", "4", "5", "6", "x", "1/x", "1", "2", "3", "-", "=", "0", ".", "+"]
+BUTTON_LABELS =["MC", "MR", "MS", "M+", "M-", "AC", " C ", "+/-", "^2", "^y", " 7 ", " 8 ", " 9 ", " / ", "sqrt", " 4 ", " 5 ", " 6 ", " x ", "1/x", " 1 ", " 2 ", " 3 ", " - ", "=", "0", " . ", "+"]
 
 def main():
-  root = Tk()
+    root = Tk()
+    root.grid_rowconfigure(1, weight=1)
 
-  canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg=CANVAS_COLOR)
-  canvas.pack()
+    canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg=CANVAS_COLOR)
+    canvas.place(relx=0.5, rely=0.0 , relwidth=1, relheight=1, anchor=DISPLAY_ANCHOR)
 
-  display_frame = Frame(canvas, bg=DISPLAY_COLOR)
-  display_frame.place(relx=DISPLAY_X, rely=DISPLAY_Y , relwidth=DISPLAY_WIDTH, relheight=DISPLAY_HEIGHT, anchor=DISPLAY_ANCHOR)
+    display_frame = Frame(canvas, bg=DISPLAY_COLOR)
+    display_frame.place(relx=DISPLAY_X, rely=DISPLAY_Y , relwidth=DISPLAY_WIDTH, relheight=DISPLAY_HEIGHT, anchor=DISPLAY_ANCHOR)
 
-  button_frame = Frame(canvas, bg=BUTTONfRAME_COLOR)
-  button_frame.place(relx=BUTTONfRAME_X, rely=BUTTONfRAME_Y , relwidth=BUTTONfRAME_WIDTH, relheight=BUTTONfRAME_HEIGHT, anchor=BUTTONfRAME_ANCHOR)
+    display_label = Label(display_frame, text = "0", font=10, bg=DISPLAY_COLOR)
+    display_label.place(relx=DISPLAY_X, rely=0.95 , relwidth=0.95, relheight=0.95, anchor='sw')
 
-  # button1 = Button(button_frame, text="MC", bg="gray", font=BUTTON_FONT)
-  # button1.place(relx=SINGLEBUTTON_X, rely=SINGLEBUTTON_Y, width=SINGLEBUTTON_WIDTH, height=SINGLEBUTTON_HEIGHT)
+    button_frame = Frame(canvas, bg=BUTTONfRAME_COLOR)
+    button_frame.place(relx=BUTTONfRAME_X, rely=BUTTONfRAME_Y , relwidth=BUTTONfRAME_WIDTH, relheight=BUTTONfRAME_HEIGHT, anchor=BUTTONfRAME_ANCHOR)
 
-  # button2 = Button(button_frame, text="MR", bg="gray", font=BUTTON_FONT)
-  # button2.place(relx=SINGLEBUTTON_X + 0.19, rely=SINGLEBUTTON_Y, width=SINGLEBUTTON_WIDTH, height=SINGLEBUTTON_HEIGHT)
+    buttons = {}
 
-  # button3 = Button(button_frame, text="MR", bg="gray", font=BUTTON_FONT)
-  # button3.place(relx=DOUBLEBUTTON_X, rely=DOUBLEBUTTON_Y + 0.09, width=DOUBLEBUTTON_WIDTH, height=DOUBLEBUTTON_HEIGHT)
+    for button_text in BUTTON_LABELS:
+        current_button = Button(button_frame, text=button_text, bg=SINGLEBUTTON_COLOR, font=BUTTON_FONT)
+        if(button_text == "0"):
+            buttons[current_button] = "0"
 
-  button1 = Button(button_frame, text="MC", bg="gray", font=BUTTON_FONT)
-  button1.grid(column=0, row=0, ipadx=4, ipady=4, padx=2, pady=1.5)
+        elif(button_text == "="):
+            buttons[current_button] = "="
 
-  button2 = Button(button_frame, text="MR", bg="gray", font=BUTTON_FONT)
-  button2.grid(column=1, row=0, ipadx=4, ipady=4, padx=2, pady=1.5)
+        else:
+            buttons[current_button] = None
 
-  button3 = Button(button_frame, text="MS", bg="gray", font=BUTTON_FONT)
-  button3.grid(column=2, row=0, columnspan=1, ipadx=4.5, ipady=4, padx=2, pady=1.5)
+    row_count, column_count = 1, 1
 
-  button4 = Button(button_frame, text="M+", bg="gray", font=BUTTON_FONT)
-  button4.grid(column=3, row=0, columnspan=1, ipadx=5.5, ipady=4, padx=2, pady=1.5)
+    for button in buttons:
+        if(buttons[button] == "0"):
+            button.grid(column=column_count, row=row_count, columnspan=2, ipadx=45, ipady=5, padx=1.5, pady=3)
+            column_count += 1
 
-  button5 = Button(button_frame, text="M-", bg="gray", font=BUTTON_FONT)
-  button5.grid(column=4, row=0, columnspan=1, ipadx=8, ipady=4, padx=2, pady=1.5)
+        elif(buttons[button] == "="):
+            button.grid(column=column_count, row=row_count, rowspan=2, ipadx=14, ipady=28, padx=1.5, pady=3)
 
-  long_button = Button(button_frame, text="  =  ", bg="gray", font=BUTTON_FONT)
-  long_button.grid(column=0, row=1, columnspan=2, ipadx=28, ipady=4, padx=1.5, pady=3)
+        elif(buttons[button] == "sqrt"):
+            button.grid(column=column_count, row=row_count, rowspan=2, ipadx=0, ipady=-4, padx=1.5, pady=3)
+
+        else:
+            button.grid(column=column_count, row=row_count, ipadx=10, ipady=4, padx=1.5, pady=3)
+
+        column_count += 1
+
+        if column_count > 5:
+            column_count = 1
+            row_count += 1
 
 
-
-  root.mainloop()
+    root.mainloop()
 
 if __name__ == 'main':
   main()
